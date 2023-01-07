897ab35425b24beff62906a1ddfab5526c057cc2
sebastien sorrentino <sebastien.sorrentino23@gmail.com>
2023-01-07 14:43:27 +0100

    CRUDWATCHlistmoviesBrailsV4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "pages#home"
+  resources :movies
   # Defines the root path route ("/")
   # root "articles#index"
 end
